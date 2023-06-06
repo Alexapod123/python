815877bc672cdbe1d292d2416a2ff45bb654d00d
Alexa <alex_krim99@mail.ru>
2023-06-06 17:21:04 +0300

    2.3

--- seminar_2/tasks.py
+++ seminar_2/tasks.py
@@ -15,18 +15,32 @@
 
 # вход число, выход каким число по счету в ряду фибоначчи,если нет, то выход -1
 
-n = int(input('Введите число: '))
-a = 0
-b = 1
-count = 2
-while b <= n:
-    if b == n:
-        print(count)
-        break
-    a,b = b, a+b
-    count+=1
-else:
-    print(-1)
+# n = int(input('Введите число: '))
+# a = 0
+# b = 1
+# count = 2
+# while b <= n:
+#     if b == n:
+#         print(count)
+#         break
+#     a,b = b, a+b
+#     count+=1
+# else:
+#     print(-1)
 
 
+# вход количество дней и темпартура в день, выход самая длинная оттпепель сколько дней
 
+n = int(input('Введите количество дней: '))
+count = 0
+max_count = 0
+
+for n in range (1, n+1):
+    tem = int(input("Введите температуру: "))
+    if tem > 0:
+        count+=1
+    elif tem<0:
+        count=0
+    if count>max_count:
+        max_count=count
+print(max_count)
