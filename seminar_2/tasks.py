2cafab0b443e078fae776bd1c91cbab3fb66d2bc
Alexa <alex_krim99@mail.ru>
2023-06-06 17:03:42 +0300

    2.2

--- seminar_2/tasks.py
+++ seminar_2/tasks.py
@@ -15,4 +15,18 @@
 
 # вход число, выход каким число по счету в ряду фибоначчи,если нет, то выход -1
 
+n = int(input('Введите число: '))
+a = 0
+b = 1
+count = 2
+while b <= n:
+    if b == n:
+        print(count)
+        break
+    a,b = b, a+b
+    count+=1
+else:
+    print(-1)
+
+
 
