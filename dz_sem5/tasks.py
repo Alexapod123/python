3c8734f47d40d4f3be42c13355a3b85dda46d9e4
Alexa <alex_krim99@mail.ru>
2023-06-14 18:08:53 +0300

    task 2

--- dz_sem5/tasks.py
+++ dz_sem5/tasks.py
@@ -4,13 +4,34 @@
 # A = 3; B = 5 -> 243 (3⁵)
 #     A = 2; B = 3 -> 8 
 
-def stepen(a, b):
-    if b==1:
-        return a
-    return (a*stepen(a,b-1))
+# def stepen(a, b):
+#     if b==1:
+#         return a
+#     return (a*stepen(a,b-1))
+    
+
+# a = int(input('введите число а: '))
+# b = int(input('ввелите число b: '))
+
+# print(stepen(a,b))
+
+#  Напишите рекурсивную функцию sum(a, b), возвращающую сумму двух целых неотрицательных чисел. 
+# Из всех арифметических операций допускаются только +1 и -1. Также нельзя использовать циклы.
+# *Пример:*
+# 2 2
+#     4 
+
+def sum(a, b):
+    if b > 0:
+        if b==1:
+            return a+1
+        return sum(a,b-1)+1
+    else:
+        if b == -1:
+            return a-1
+        return sum(a, b+1)-1
     
 
 a = int(input('введите число а: '))
 b = int(input('ввелите число b: '))
-
-print(stepen(a,b))
+print(sum(a,b))
